2b8400a9835c45305477f2dcf32892d8d54039ae
atsn.ngs <atsn.ngs@9414a476-3a48-0410-b3e9-4b25688ab2d7>
2008-03-12 16:27:15 +0000

    git-svn-id: https://noawidget.googlecode.com/svn/trunk@25 9414a476-3a48-0410-b3e9-4b25688ab2d7

---
+++ export.sh
@@ -0,0 +1,3 @@
+svn ci -m "auto commit" "./Now On Air.wdgtproj/project.wdgt"
+svn export --force "./Now On Air.wdgtproj/project.wdgt" "./Now On Air.wdgt"
+svn ci -m "auto commit" "./Now On Air.wdgt"
