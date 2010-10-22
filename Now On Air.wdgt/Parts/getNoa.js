f28a53f9a79e3dafdf3938e34e5a56a531e9a880
atsn.ngs <atsn.ngs@9414a476-3a48-0410-b3e9-4b25688ab2d7>
2010-10-22 18:06:25 +0000

    itunes
    git-svn-id: https://noawidget.googlecode.com/svn/trunk@34 9414a476-3a48-0410-b3e9-4b25688ab2d7

--- Now On Air.wdgt/Parts/getNoa.js
+++ Now On Air.wdgt/Parts/getNoa.js
@@ -3,10 +3,10 @@
 :::
 :::
 :::
-:::	 Now On Air for Dashboard 1.2 :::
+:::     Now On Air for Dashboard 1.2 :::
 :::
-:::	 code + design ::: atsushi nagase ::: http://ngsdev.org/
-:::	 Copyright 2005-2008 atsushi nagase. All rights  reserved.
+:::     code + design ::: atsushi nagase ::: http://ngsdev.org/
+:::     Copyright 2005-2008 atsushi nagase. All rights  reserved.
 :::
 :::
 :::
@@ -27,8 +27,10 @@ _gaq.push(['_trackPageview', '/load']);
 
 
 var xmlloc = "http://www.j-wave.co.jp/top/xml/now_on_air_song.xml";
-var cdloc = "https://www.amazon.co.jp/s/?tag=atsushnagased-22&search-alias=popular&__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&field-keywords=";
-var query
+var cdloc = "http://ax.search.itunes.apple.com/WebObjects/MZSearch.woa/wa/search?entity=album&restrict=true&submit=seeAllLockups&term=";
+var affConvert = "http://feed.linksynergy.com/createcustomlink.shtml?token=fafab0b82dd76fc58c5bf929a42147961f871c911297c98217949be9b68b123f&mid=13894&murl=";
+var cdURL;
+var query;
 var statusDiv;
 var noaDiv;
 var interval_engine;
@@ -42,138 +44,149 @@ function setup() {
     if (setup.called) return;
     setup.called = true;
     CreateGlassButton('doneButton', { text: '完了' });
-	flip2 = new Flip("flip2","fliprollie2");
-	flip = new Flip("flip","fliprollie");
-	if (window.widget) {
-		widget.onhide = onhide;
-		widget.onshow = onshow;
-		interval = parseInt(widget.preferenceForKey("interval"));
-	}
-	if(isNaN(interval)) changeInterval(1);
-	getNoa();
+    flip2 = new Flip("flip2","fliprollie2");
+    flip = new Flip("flip","fliprollie");
+    if (window.widget) {
+        widget.onhide = onhide;
+        widget.onshow = onshow;
+        interval = parseInt(widget.preferenceForKey("interval"));
+    }
+    if(isNaN(interval)) changeInterval(1);
+    getNoa();
 }
 
 function onshow() {
-	activeInterval()
-	getNoa()
+    activeInterval()
+    getNoa()
 }
 
 function activeInterval() {
-	removeInterval()
-	if(interval) interval_engine = setInterval("getNoa();", interval);
+    removeInterval()
+    if(interval) interval_engine = setInterval("getNoa();", interval);
 }
 
 function showFlips() {
-	flip2.button.style.backgroundImage = "url(images/rotate.png)";
-	flip.show()
-	flip2.show()
+    flip2.button.style.backgroundImage = "url(images/rotate.png)";
+    flip.show()
+    flip2.show()
 }
 
 function hideFlips() {
-	flip.hide()
-	flip2.hide()
+    flip.hide()
+    flip2.hide()
 }
 
 function removeInterval() {
-	clearInterval(interval_engine);
-	delete interval_engine;
+    clearInterval(interval_engine);
+    delete interval_engine;
 }
 
 function onhide() {
-	removeInterval()
+    removeInterval()
+}
+
+function doRequest(url,callback) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if(xhttp.readyState == 4){
+            flip2.button.style.backgroundImage = "url(images/rotate.gif)";
+            if(! flip2.isOver()) flip2.opacityTo(0)
+            if(xhttp.responseText) {
+                callback.apply(xhttp,[xhttp.responseText]);
+            }
+        }
+    }    
+    xhttp.setRequestHeader('Content-Type', 'text/xml;charset=utf-8');
+    xhttp.open('GET',url,true)
+    xhttp.send(null);
 }
 
 function getNoa() {
-	noaDiv = document.getElementById("noa");
-	flip2.setOpacity(100)
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		if(xhttp.readyState == 4){
-			flip2.button.style.backgroundImage = "url(images/rotate.gif)";
-			if(! flip2.isOver()) flip2.opacityTo(0)
-			if(xhttp.responseText) {
-				showStatus();
-				changeNoa(xhttp.responseXML);
-			}
-		}
-	}
-	var rnd = Math.ceil(Math.random()*100000);
-	var fname = xmlloc+"?rnd="+rnd;
-	xhttp.setRequestHeader('Content-Type', 'text/xml;charset=utf-8');
-	xhttp.open('GET',fname,true)
-	xhttp.send(null);
+    noaDiv = document.getElementById("noa");
+    flip2.setOpacity(100)
+    var rnd = Math.ceil(Math.random()*100000);
+    doRequest(xmlloc+"?rnd="+rnd,function(){
+        showStatus();
+        changeNoa(this.responseXML);
+    });
 }
 var song, artist;
 function changeNoa(param) {
-	var fc = param.firstChild.childNodes
-	var len = fc.length
-	for(var i=0;i<len;i++) {
-		if(fc[i].nodeName=="data") {
-			var info = fc[i].getAttribute("information");
-			var lar = fc[i].getAttribute("cd_url").split("?noa=");
-			//cdloc = lar[0];
-			var info_ar = info.split("」")
-			song = info_ar[0].split("「").join('');
-			artist = info_ar[1]
-			noaDiv.innerHTML = "<div id=\"song\">"+song+"</div>";
-			noaDiv.innerHTML += "<div id=\"artist\">"+artist+ "</div>";
-			query = song+" "+artist.replace(/(:?\s*)(\d{1,2}\:\d{1,2})(:?\s*)/,"");
-			break;
-		}
-	}
+    var fc = param.firstChild.childNodes
+    var len = fc.length
+    for(var i=0;i<len;i++) {
+        if(fc[i].nodeName=="data") {
+            var info = fc[i].getAttribute("information");
+            var lar = fc[i].getAttribute("cd_url").split("?noa=");
+            //cdloc = lar[0];
+            var info_ar = info.split("」")
+            song = info_ar[0].split("「").join('');
+            song = song.replace(/[（\(]live[）|\)]/ig,"")
+            artist = info_ar[1]
+            noaDiv.innerHTML = "<div id=\"song\">"+song+"</div>";
+            noaDiv.innerHTML += "<div id=\"artist\">"+artist+ "</div>";
+            query = song+" "+artist.replace(/(:?\s*)(\d{1,2}\:\d{1,2})(:?\s*)/,"");
+            doRequest(affConvert+encodeURI(cdloc+encodeURIComponent(query)),function(res) {
+                res = (res||"").replace(/[\s\n\r]/g);
+                if(!res) return;
+                res = res.replace(/^([^\?]+)\?([^\?]+)\?(.*)$/,"$1?$2&$3");
+                cdURL = res;
+            });
+            break;
+        }
+    }
     _gaq.push(['_trackPageview', '/'+artist+'/'+song]);
 }
 function showStatus(param) {
-	if(!param) param = "";
-	statusDiv = document.getElementById("status");
-	statusDiv.innerHTML = param;
+    if(!param) param = "";
+    statusDiv = document.getElementById("status");
+    statusDiv.innerHTML = param;
 }
 function getCD() {
-    var url = cdloc+encodeURIComponent(query);
     _gaq.push(['_trackPageview', '/getCD/'+query]);
     _gaq.push(['_trackEvent', 'getCD', query]);
-	if(!flip.isOver()&&!flip2.isOver()&&!_backmode) {
-		if (window.widget) widget.openURL(url);
-		else window.location.href = url;
-	}
+    if(!flip.isOver()&&!flip2.isOver()&&!_backmode) {
+        if (window.widget) widget.openURL(cdURL);
+        else window.location.href = cdURL;
+    }
 }
 
 function changeInterval(t) {
-	t = interval_array[t];
-	t*=60000;
-	if(t) {
-		activeInterval()
-	} else {
-		t = 0;
-		removeInterval()
-	}
-	interval = t;
-	if(window.widget) widget.setPreferenceForKey(t,"interval");
+    t = interval_array[t];
+    t*=60000;
+    if(t) {
+        activeInterval()
+    } else {
+        t = 0;
+        removeInterval()
+    }
+    interval = t;
+    if(window.widget) widget.setPreferenceForKey(t,"interval");
 }
 
 function attachSelect() {
-	var sel = document.getElementById("selectInterval");
-	var len = interval_array.length;
-	if(window.widget) interval = parseInt(widget.preferenceForKey("interval"));
-	for(var i=0; i<len; i++) {
-		var opt = document.createElement("option");
-		opt.value = i.toString();
-		opt.selected = interval_array[i]*60000 == interval;
-		var m = Math.floor(interval_array[i]);
-		var s = interval_array[i] - m
-		if(s>0) s*=60;
-		var t = "";
-		if(m) t+= m.toString()+"分";
-		if(s) t+= s.toString()+"秒";
-		if(!t) t= "しない";
-		var tx = document.createTextNode(t)
-		opt.appendChild(tx)
-		sel.appendChild(opt)
-	}
+    var sel = document.getElementById("selectInterval");
+    var len = interval_array.length;
+    if(window.widget) interval = parseInt(widget.preferenceForKey("interval"));
+    for(var i=0; i<len; i++) {
+        var opt = document.createElement("option");
+        opt.value = i.toString();
+        opt.selected = interval_array[i]*60000 == interval;
+        var m = Math.floor(interval_array[i]);
+        var s = interval_array[i] - m
+        if(s>0) s*=60;
+        var t = "";
+        if(m) t+= m.toString()+"分";
+        if(s) t+= s.toString()+"秒";
+        if(!t) t= "しない";
+        var tx = document.createTextNode(t)
+        opt.appendChild(tx)
+        sel.appendChild(opt)
+    }
 }
 
 function removeSelect() {
-	document.getElementById("selectInterval").innerHTML = "";
+    document.getElementById("selectInterval").innerHTML = "";
 }
 
 
@@ -187,32 +200,32 @@ http://developer.apple.com/ja/documentation/AppleApplications/Conceptual/Dashboa
 ------------------------------------------------------------------------*/
 function showBack()
 {
-	_backmode = true;
-	attachSelect();
-	var front = document.getElementById("front");
-	var back = document.getElementById("back");
-	if (window.widget) widget.prepareForTransition("ToBack");
-	front.style.display="none";
-	back.style.display="block";
-	if (window.widget) setTimeout ('widget.performTransition();', 0);  
+    _backmode = true;
+    attachSelect();
+    var front = document.getElementById("front");
+    var back = document.getElementById("back");
+    if (window.widget) widget.prepareForTransition("ToBack");
+    front.style.display="none";
+    back.style.display="block";
+    if (window.widget) setTimeout ('widget.performTransition();', 0);  
     _gaq.push(['_trackPageview', '/back']);
 }
 
 
 function hideBack()
 {
-	_backmode = false;
-	changeInterval(document.getElementById("selectInterval").value);
-	flip.out(event);
-	flip2.out(event);
-	removeSelect();
-	activeInterval()
-	var front = document.getElementById("front");
-	var back = document.getElementById("back");
-	if (window.widget) widget.prepareForTransition("ToFront");
-	back.style.display="none";
-	front.style.display="block";
-	if (window.widget) setTimeout ('widget.performTransition();', 0);
+    _backmode = false;
+    changeInterval(document.getElementById("selectInterval").value);
+    flip.out(event);
+    flip2.out(event);
+    removeSelect();
+    activeInterval()
+    var front = document.getElementById("front");
+    var back = document.getElementById("back");
+    if (window.widget) widget.prepareForTransition("ToFront");
+    back.style.display="none";
+    front.style.display="block";
+    if (window.widget) setTimeout ('widget.performTransition();', 0);
     _gaq.push(['_trackPageview', '/front']);
 }
 
